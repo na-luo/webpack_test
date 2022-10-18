0d4158331ce3078bc7868e86c79574959a9e6b9c
na-luo <942210530@qq.com>
2022-10-18 19:50:14 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 var path = require('path');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'My App',
+    template:'src/assets/index.html'     
   }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader'
+           
+          }
+        ]
+      }
+    ]
+  }
+  
 };
